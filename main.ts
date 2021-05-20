31098f1425a292d0656c8562b6701dc63fee50c7
robotescu <80849623+robotescu@users.noreply.github.com>
2021-05-20 19:49:14 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+input.onGesture(Gesture.TiltLeft, function () {
+    servos.P2.setAngle(0)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    servos.P2.setAngle(90)
+})
 let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
+basic.showIcon(IconNames.Chessboard)
 basic.forever(function () {
     strip.show()
     strip.rotate(1)
